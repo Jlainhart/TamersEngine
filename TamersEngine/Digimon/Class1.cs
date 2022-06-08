431bd6012a8758955807e258ddda088afa704838
Jlainhart <103013523+Jlainhart@users.noreply.github.com>
2022-06-08 12:55:31 -0400

    Add files via upload

---
+++ TamersEngine/Digimon/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Timers;
+
+namespace TamersEngine
+{
+    public class Basestats
+    {
+        public string userName { get; set; }
+        public string digiName { get; set; }
+        public int lvl { get; set; }
+        public int hp { get; set; }
+        public int mp { get; set; }
+        public int atk { get; set; }
+        public int def { get; set; }
+        public int intel { get; set; }
+        public int spd { get; set; }
+        public int energy { get; set; }
+        public int vitality { get; set; }
+        public int exp { get; set; }
+        public int mood { get; set; }
+        public int tired { get; set; }
+        public int sleep { get; set; }
+        public int hunger { get; set; }
+        public int poop { get; set; }
+        public int age { get; set; }
+        public int minAge { get; set; }
+        public int maxAge { get; set; }
+        public string digi { get; set; }
+        public string type { get; set; }
+        public string attribute { get; set; }
+        public string species { get; set; }
+        public string personality { get; set; }
+    }
+}
+
