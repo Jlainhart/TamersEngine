0d63e12fb81882a3eef163b25f4ec9208c173e47
Jedadiah Lainhart <jedadiah.lainhart@gmail.com>
2022-07-19 19:56:50 -0400

    .

--- TamersEngine/Digi.cs
+++ TamersEngine/Digi.cs
@@ -13,14 +13,29 @@ using Value;
 
 namespace Digis
 {
-	public class Mons()
+	public class Mons
 	{
-		public static void Botamon()
+        public static void Botamon()
         {
-        Values.digi = Botamon;
-        Values.type = Data;
-        Values.attribute = Neutral;
-        Values.species = Demon;
+            Values.digi = Botamon;
+            Values.type = Data;
+            Values.attribute = Neutral;
+            Values.species = Demon;
         }
+
+            public static void BotamonBaby()
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("                  ■ ■■■■ ■   ");
+                Console.WriteLine("                ■          ■ ");
+                Console.WriteLine("               ■  ■      ■  ■");
+                Console.WriteLine("               ■    ■■■■    ■");
+                Console.WriteLine("               ■            ■");
+                Console.WriteLine("                ■          ■ ");
+                Console.WriteLine("                  ■ ■■■■ ■   ");
+            }
+        
     }
 }
