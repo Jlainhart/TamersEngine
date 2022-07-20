1d20c9840d7f8fd9ca5d2a06d9a32fd790b4d722
Jedadiah Lainhart <jedadiah.lainhart@gmail.com>
2022-07-19 20:41:34 -0400

    improved Mons and add random Mon for egg type

--- TamersEngine/Digi.cs
+++ TamersEngine/Digi.cs
@@ -17,10 +17,10 @@ namespace Digis
 	{
         public static void Botamon()
         {
-            Values.digi = Botamon;
-            Values.type = Data;
-            Values.attribute = Neutral;
-            Values.species = Demon;
+            Values.digi = "Botamon";
+            Values.type = "Data";
+            Values.attribute = "Dark";
+            Values.species = "Demon";
         }
 
             public static void BotamonBaby()
@@ -36,6 +36,69 @@ namespace Digis
                 Console.WriteLine("                ■          ■ ");
                 Console.WriteLine("                  ■ ■■■■ ■   ");
             }
-        
+        public static void Chibomon()
+        {
+            Values.digi = "Chibomon";
+            Values.type = "Free";
+            Values.attribute = "Fire";
+            Values.species = "Dragon";
+        }
+
+        public static void ChibomonBaby()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("                  ■ ■■■■ ■   ");
+            Console.WriteLine("                ■          ■ ");
+            Console.WriteLine("               ■  ■      ■  ■");
+            Console.WriteLine("               ■    ■■■■    ■");
+            Console.WriteLine("               ■            ■");
+            Console.WriteLine("                ■          ■ ");
+            Console.WriteLine("                  ■ ■■■■ ■   ");
+        }
+        public static void Jyarimon()
+        {
+            Values.digi = "Jyarimon";
+            Values.type = "Virus";
+            Values.attribute = "Fire";
+            Values.species = "Dragon";
+        }
+
+        public static void JyarimonBaby()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("                  ■ ■■■■ ■   ");
+            Console.WriteLine("                ■          ■ ");
+            Console.WriteLine("               ■  ■      ■  ■");
+            Console.WriteLine("               ■    ■■■■    ■");
+            Console.WriteLine("               ■            ■");
+            Console.WriteLine("                ■          ■ ");
+            Console.WriteLine("                  ■ ■■■■ ■   ");
+        }
+        public static void Punimon()
+        {
+            Values.digi = "Punimon";
+            Values.type = "Data";
+            Values.attribute = "Neutral";
+            Values.species = "Beast";
+        }
+
+        public static void PunimonBaby()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("                  ■ ■■■■ ■   ");
+            Console.WriteLine("                ■          ■ ");
+            Console.WriteLine("               ■  ■      ■  ■");
+            Console.WriteLine("               ■    ■■■■    ■");
+            Console.WriteLine("               ■            ■");
+            Console.WriteLine("                ■          ■ ");
+            Console.WriteLine("                  ■ ■■■■ ■   ");
+        }
+
     }
 }
